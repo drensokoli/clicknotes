bf62f730b7d33deeb2d07f4829b3b1ab90f7831b
dren.s <dren.s@gjirafa.com>
2023-04-30 01:08:40 +0200

    Added button to movies container

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,4 +1,3 @@
-// components/Movie.tsx
 import React from 'react';
 
 interface MovieProps {
@@ -11,24 +10,33 @@ interface MovieProps {
 
 const Movie: React.FC<MovieProps> = ({ id, title, release_date, poster_path, onClick }) => {
   return (
-    <div key={id} className="movie-card" onClick={onClick}>
-      {poster_path ? (
-        <img
-          src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-          alt={title}
-          className="movie-image h-[300px] shadow-2xl rounded"
-        />
-      ) : (
-        <div className=' bg-white bg-opacity-10 backdrop-blur-lg rounded drop-shadow-md'
-          style={{
-            width: '200px',
-            height: '300px',
-            borderRadius: '5px'
-          }}
-        />
-      )}
-      <h2 className="text-l font-bold text-center text-gray-800">
-        {title} {release_date ? ` (${release_date.split('-')[0]})` : ''}
+    <div key={id} className="movie-card ">
+      <div className="movie-card-image-container">
+
+        {poster_path ? (
+          <div className='movie-image'>
+
+            <img
+              src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+              alt={title}
+              className="h-[300px] rounded-sm"
+            />
+            <button type="button" className="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900 movie-card-button">Add to Notion</button>
+          </div>
+        ) : (
+          <div className=' bg-white movie-image'
+            style={{
+              width: '200px',
+              height: '300px',
+              borderRadius: '5px'
+            }}
+          >
+            <button type="button" className="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900 movie-card-button">Add to Notion</button>
+          </div>
+        )}
+      </div>
+      <h2 className="text-l font-bold text-center text-gray-800 movie-title">
+        <span onClick={onClick}>{title} {release_date ? ` (${release_date.split('-')[0]})` : ''}</span>
       </h2>
     </div>
   );
