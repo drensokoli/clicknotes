d951f97be216c79ba59555f041718afb5685db46
dren.s <dren.s@gjirafa.com>
2023-04-30 01:14:42 +0200

    Styled missing poster_path movies

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -24,7 +24,7 @@ const Movie: React.FC<MovieProps> = ({ id, title, release_date, poster_path, onC
             <button type="button" className="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900 movie-card-button">Add to Notion</button>
           </div>
         ) : (
-          <div className=' bg-white movie-image'
+          <div className=' bg-transparent movie-image backdrop-blur-sm'
             style={{
               width: '200px',
               height: '300px',
