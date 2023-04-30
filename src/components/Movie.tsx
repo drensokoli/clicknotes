d5c5960657bd5c12bc6326a0f810f928f10535a0
dren.s <dren.s@gjirafa.com>
2023-04-30 10:25:37 +0200

    Added Tv Shows to search results

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -20,23 +20,26 @@ const Movie: React.FC<MovieProps> = ({ id, title, release_date, poster_path, onC
               src={`https://image.tmdb.org/t/p/w500${poster_path}`}
               alt={title}
               className="h-[300px] rounded-sm"
+              onClick={onClick}
             />
             <button type="button" className="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900 movie-card-button">Add to Notion</button>
           </div>
         ) : (
-          <div className=' bg-transparent movie-image backdrop-blur-sm'
-            style={{
-              width: '200px',
-              height: '300px',
-              borderRadius: '5px'
-            }}
-          >
+          <div className='movie-image'>
+            <div className="bg-transparent backdrop-blur-sm"
+              style={{
+                width: '200px',
+                height: '300px',
+                borderRadius: '5px'
+              }}
+              onClick={onClick}
+            ></div>
             <button type="button" className="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900 movie-card-button">Add to Notion</button>
           </div>
         )}
       </div>
-      <h2 className="text-l font-bold text-center text-gray-800 movie-title">
-        <span onClick={onClick}>{title} {release_date ? ` (${release_date.split('-')[0]})` : ''}</span>
+      <h2 className="text-l font-bold text-center text-gray-800">
+        <span>{title} {release_date ? ` (${release_date.split('-')[0]})` : ''}</span>
       </h2>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,18 @@ import axios from 'axios';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import Movie from '@/components/Movie';
+import TVShow from '@/components/TVShow';
+
+interface TVShow {
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string;
+  vote_average: number;
+  vote_count: number;
+  first_air_date: string;
+}
+
 
 const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
@@ -19,45 +31,77 @@ interface Movie {
 export default function Home() {
   const [input, setInput] = useState('');
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [tvShows, setTvShows] = useState<TVShow[]>([]);
 
   const handleInputChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     setInput(event.target.value);
-    await searchMoviesByTitle(event.target.value);
+    await searchContentByTitle(event.target.value);
   };
 
-  const searchMoviesByTitle = async (title: string) => {
+  const searchContentByTitle = async (title: string) => {
     try {
-      const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${title}`);
-      setMovies(response.data.results);
+      const movieResponse = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${title}`);
+      setMovies(movieResponse.data.results);
+
+      const tvShowResponse = await axios.get(`https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&query=${title}`);
+      setTvShows(tvShowResponse.data.results);
     } catch (error) {
       console.error(error);
     }
   };
+  // const handleInputChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setInput(event.target.value);
+  //   await searchMoviesByTitle(event.target.value);
+  // };
+
+  // const searchMoviesByTitle = async (title: string) => {
+  //   try {
+  //     const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${title}`);
+  //     setMovies(response.data.results);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+  // pages/index.tsx
+
+  const handleTVShowClick = (tvShowId: number) => {
+    window.open(`https://www.themoviedb.org/tv/${tvShowId}`, '_blank');
+  };
+
 
   const handleMovieClick = (movieId: number) => {
     window.open(`https://www.themoviedb.org/movie/${movieId}`, '_blank');
   };
 
+  // pages/index.tsx
+
   return (
     <>
       <div>
         <Navbar input={input} handleInputChange={handleInputChange} />
-        <div className="flex flex-col items-center justify-center min-h-screen bg-peach space-y-4 pt-40 md:pt-28 background-image">
-          <div className="movie-container lg:w-5/6">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                title={movie.title}
-                release_date={movie.release_date}
-                poster_path={movie.poster_path}
-                onClick={() => handleMovieClick(movie.id)}
-              />
-            ))}
+        <div className="flex flex-col items-center justify-center min-h-screen bg-peach space-y-4 pt-28 m:pt-40 background-image">
+          <div className="content-container lg:w-5/6">
+            {movies.length > 0 && (
+              <h2 className="text-xl font-bold mb-4 text-center">Movies</h2>
+            )}
+            <div className="movie-container">
+              {movies.map((movie) => (
+                <Movie key={movie.id} {...movie} onClick={() => handleMovieClick(movie.id)} />
+              ))}
+            </div>
+            {tvShows.length > 0 && (
+              <h2 className="text-xl font-bold mb-4 mt-8 text-center">TV Shows</h2>
+            )}
+            <div className="movie-container">
+              {tvShows.map((tvShow) => (
+                <TVShow key={tvShow.id} {...tvShow} onClick={() => handleTVShowClick(tvShow.id)} />
+              ))}
+            </div>
           </div>
         </div>
         <Footer />
       </div>
     </>
   );
+
 }
