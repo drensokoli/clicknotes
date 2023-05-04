0b176f28fe00aff1049ca6bb0f83f17cc4217996
Dren Sokoli <dren.s@gjirafa.com>
2023-05-04 16:53:04 +0200

    Commented imdb link and deleted unnecessary media

--- src/components/PageContent.tsx
+++ src/components/PageContent.tsx
@@ -45,14 +45,14 @@ const PageContent: React.FC<PageContentProps> = ({ contentType }) => {
 
     const handleContentClick = async (contentId: number) => {
         try {
-            const response = await axios.get(`https://api.themoviedb.org/3/${contentType}/${contentId}?api_key=${API_KEY}`);
-            const imdbId = response.data.imdb_id;
-            console.log(imdbId);
-            if (imdbId) {
-                window.open(`https://www.imdb.com/title/${imdbId}`, '_blank');
-            } else {
+            // const response = await axios.get(`https://api.themoviedb.org/3/${contentType}/${contentId}?api_key=${API_KEY}`);
+            // const imdbId = response.data.imdb_id;
+            // console.log(imdbId);
+            // if (imdbId) {
+            //     window.open(`https://www.imdb.com/title/${imdbId}`, '_blank');
+            // } else {
                 window.open(`https://www.themoviedb.org/${contentType}/${contentId}`, '_blank');
-            }
+            // }
         } catch (error) {
             console.error(error);
         }
