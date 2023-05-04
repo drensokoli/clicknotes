371334d6a1f9210deb9579dcc1121b2b1c451176
dren.s <dren.s@gjirafa.com>
2023-05-04 18:13:33 +0200

    Updated profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -28,63 +28,60 @@ export default function Profile() {
 
     return (
         <>
-            <div className="antialiased mt-24">
+            <div className="antialiased mt-24 flex justify-center items-center">
                 <div className="container">
-                    <div>
+                    <div className="bg-white relative w-5/6 md:w-5/6  lg:w-4/6 xl:w-3/6 mx-auto rounded-md drop-shadow-[0_0_10px_rgba(0,0,0,0.2)]">
+                        <div className="flex justify-center">
+                            <img src={session?.user?.image?.toString()} alt="" className="rounded-full mx-auto absolute -top-20 w-32 h-32 shadow-2xl border-4 border-white transition duration-200 transform hover:scale-110 " />
+                        </div>
 
-                        <div className="bg-white relative w-5/6 md:w-5/6  lg:w-4/6 xl:w-3/6 mx-auto rounded-md drop-shadow-[0_0_10px_rgba(0,0,0,0.2)] ">
-                            <div className="flex justify-center">
-                                <img src={session?.user?.image?.toString()} alt="" className="rounded-full mx-auto absolute -top-20 w-32 h-32 shadow-2xl border-4 border-white transition duration-200 transform hover:scale-110 " />
-                            </div>
+                        <div className="mt-16">
+                            <h1 className="font-bold text-center text-3xl text-gray-900">{session?.user?.name}</h1>
+                            <div className="w-full">
+                                <div className="mt-5 w-full flex flex-col items-center overflow-hidden text-sm pb-4">
+                                    <form onSubmit={handleSubmit} className='w-full pl-2 pr-2'>
+                                        <div className='mb-4 border-b-2 border-gray pb-6 pl-6 pr-6'>
+                                            <label className="block mb-2 text-sm text-gray-500">API Key</label>
+                                            <input
+                                                type="text"
+                                                value={notionApiKey}
+                                                onChange={(e) => setNotionApiKey(e.target.value)}
+                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                                placeholder="Enter your Notion API Key"
+                                            />
+                                        </div>
+                                        <div className='mb-4 pl-6 pr-6'>
+                                            <label className="block mb-2 text-sm text-gray-500">Movies Page link</label>
+                                            <input
+                                                type="text"
+                                                // value={notionApiKey}
+                                                // onChange={(e) => setNotionApiKey(e.target.value)}
+                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                                placeholder="Enter your Movies Page link"
+                                            />
+                                        </div>
+                                        <div className='mb-4 pl-6 pr-6'>
+                                            <label className="block mb-2 text-sm text-gray-500">TV Shows Page link</label>
+                                            <input
+                                                type="text"
+                                                // value={notionApiKey}
+                                                // onChange={(e) => setNotionApiKey(e.target.value)}
+                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                                placeholder="Enter your TV Shows Page link"
+                                            />
+                                        </div>
+                                        <div className='mb-4 pl-6 pr-6'>
+                                            <label className="block mb-2 text-sm text-gray-500">Books Page link</label>
+                                            <input
+                                                type="text"
+                                                // value={notionApiKey}
+                                                // onChange={(e) => setNotionApiKey(e.target.value)}
+                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                                                placeholder="Enter your Books Page link"
+                                            />
+                                        </div>
 
-                            <div className="mt-16">
-                                <h1 className="font-bold text-center text-3xl text-gray-900">{session?.user?.name}</h1>
-                                <div className="w-full">
-                                    <div className="mt-5 w-full flex flex-col items-center overflow-hidden text-sm pb-4">
-                                        <form onSubmit={handleSubmit} className='w-full pl-2 pr-2'>
-                                            <div className='mb-4 border-b-2 border-gray pb-6 pl-6 pr-6'>
-                                                <label className="block mb-2 text-sm text-gray-500">API Key</label>
-                                                <input
-                                                    type="text"
-                                                    value={notionApiKey}
-                                                    onChange={(e) => setNotionApiKey(e.target.value)}
-                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                                    placeholder="Enter your Notion API Key"
-                                                />
-                                            </div>
-                                            <div className='mb-4 pl-6 pr-6'>
-                                                <label className="block mb-2 text-sm text-gray-500">Movies Page link</label>
-                                                <input
-                                                    type="text"
-                                                    value={notionApiKey}
-                                                    onChange={(e) => setNotionApiKey(e.target.value)}
-                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                                    placeholder="Enter your Movies Page link"
-                                                />
-                                            </div>
-                                            <div className='mb-4 pl-6 pr-6'>
-                                                <label className="block mb-2 text-sm text-gray-500">TV Shows Page link</label>
-                                                <input
-                                                    type="text"
-                                                    value={notionApiKey}
-                                                    onChange={(e) => setNotionApiKey(e.target.value)}
-                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                                    placeholder="Enter your TV Shows Page link"
-                                                />
-                                            </div>
-                                            <div className='mb-4 pl-6 pr-6'>
-                                                <label className="block mb-2 text-sm text-gray-500">Books Page link</label>
-                                                <input
-                                                    type="text"
-                                                    value={notionApiKey}
-                                                    onChange={(e) => setNotionApiKey(e.target.value)}
-                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
-                                                    placeholder="Enter your Books Page link"
-                                                />
-                                            </div>
-
-                                        </form>
-                                    </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
