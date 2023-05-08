9f683dd65ce89e8b020e837823ed61157dbe1709
dren.s <dren.s@gjirafa.com>
2023-05-08 20:34:47 +0200

    Changed expiration time for bestsellers key in redis

--- src/pages/api/redisHandler.ts
+++ src/pages/api/redisHandler.ts
@@ -13,7 +13,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     res.status(200).json(data);
   } else if (req.method === 'POST') {
     const { bestsellers } = req.body;
-    await redis.set('bestsellers', JSON.stringify(bestsellers), 'EX', 60 * 60);
+    await redis.set('bestsellers', JSON.stringify(bestsellers), 'EX', 7 * 24 * 60 * 60);
     res.status(200).json({ message: 'Data stored successfully' });
   } else {
     res.status(405).json({ message: 'Method not allowed' });
