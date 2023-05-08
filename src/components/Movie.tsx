2c211b1d4a1c0b07135d231ee8f23572a1b3166c
dren.s <dren.s@gjirafa.com>
2023-05-08 20:08:35 +0200

    Redeployment

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSession, signIn, signOut } from 'next-auth/react';
+import { useSession, signIn } from 'next-auth/react';
 import Image from 'next/dist/client/image';
 
 interface MovieProps {
