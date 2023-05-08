72e623916e5a2fd05d2dfc337b838e54c557ce4a
Dren Sokoli <dren.s@gjirafa.com>
2023-05-08 15:10:44 +0200

    Fixed profile page handleSubmit function

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -28,7 +28,7 @@ export default function Profile() {
                     booksPageLink,
                 }),
             });
-            router.push('/profile');
+            router.reload();
         } catch (error) {
             console.error('Error:', error);
         }
@@ -85,7 +85,6 @@ export default function Profile() {
                                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                                                 placeholder={notionApiKey || "Enter your Notion API Key"}
                                             />
-
                                         </div>
                                         <div className='mb-4 pl-6 pr-6'>
                                             <label className="block mb-2 text-sm text-gray-500">Movies Page link</label>
