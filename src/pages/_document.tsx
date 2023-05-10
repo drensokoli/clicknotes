0339e043027f62ff8bc326b71f8374b3f7a1333b
dren.s <dren.s@gjirafa.com>
2023-05-10 13:46:04 +0200

    Bug fix buy me a coffee

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document({ title }: any) {
     <Html lang="en">
       <title>MovieNotes</title>
       <meta name="MovieNotes" content="Keeping track of your favourite movies, made easier" />
-      <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="drensokoli" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+      <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="drensokoli" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18" async></script>
       <Head />
       <body>
         <Main />
