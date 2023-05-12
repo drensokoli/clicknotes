8e7dd4913a1662aa2848cd91a6fe3f2e576291f2
dren.s <dren.s@gjirafa.com>
2023-05-12 21:40:00 +0200

    Updated addMoviesToNotion and addTvShowsToNotion to only update pages if they already exist

--- src/pages/api/addMovieToNotion.ts
+++ src/pages/api/addMovieToNotion.ts
@@ -7,93 +7,183 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
         const notion = new Client({ auth: notionApiKey });
 
-        const newPage = await notion.pages.create({
-            parent: {
-                database_id: db_id,
-            },
-            properties: {
-                'ID': {
-                    number: movieData.id,
-                },
-                'Name': {
-                    title: [
-                        {
-                            text: {
-                                content: movieData.title,
-                            },
-                        },
-                    ],
-                },
-                'Release Date': {
-                    date: {
-                        start: movieData.release_date,
-                    },
-                },
-                'Genres': {
-                    multi_select: [{"name": movieData.firstGenre},{"name": movieData.secondGenre},{"name": movieData.thirdGenre}],
-                },
-                'TMDB Rating': {
-                    number: movieData.vote_average,
-                },
-                'TMDB Link': {
-                    url: movieData.tmdb_link,
-                },
-                'iMDB Link': {
-                    url: movieData.imdb_link,
-                },
-                'Adult': {
-                    checkbox: movieData.adult,
-                },
-                'Type': {
-                    select: {
-                        name: 'Movie',
-                    },
-                },
-                'Status': {
-                    select: {
-                        name: 'To watch',
-                    },
-                },
-            },
-            icon: {
-                type: 'emoji',
-                emoji: '🎬',
-            },
-            cover: {
-                type: 'external',
-                external: {
-                    url: movieData.backdrop_path,
+        const existingPages = await notion.databases.query({
+            database_id: db_id,
+            filter: {
+                property: 'ID',
+                number: {
+                    equals: movieData.id,
                 },
             },
         });
 
-        const contentUpdateResponse = await notion.blocks.children.append({
-            block_id: newPage.id,
-            children: [
-                {
-                    object: 'block',
-                    type: 'embed',
-                    embed: {
-                        url: movieData.poster_path,
+        const genresArray = [];
+
+        if (movieData.firstGenre) {
+            genresArray.push({ "name": movieData.firstGenre });
+        }
+
+        if (movieData.secondGenre) {
+            genresArray.push({ "name": movieData.secondGenre });
+        }
+
+        if (movieData.thirdGenre) {
+            genresArray.push({ "name": movieData.thirdGenre });
+        }
+
+        if (existingPages.results.length > 0) {
+            // Update the existing page
+            const existingPageId = existingPages.results[0].id;
+
+            await notion.pages.update({
+                page_id: existingPageId,
+                properties: {
+                    'ID': {
+                        number: movieData.id,
                     },
-                },
-                {
-                    object: 'block',
-                    type: 'paragraph',
-                    paragraph: {
-                        rich_text: [
+                    'Name': {
+                        title: [
                             {
-                                type: 'text',
                                 text: {
-                                    content: movieData.overview,
+                                    content: movieData.title,
                                 },
                             },
                         ],
                     },
+                    'Release Date': {
+                        date: {
+                            start: movieData.release_date,
+                        },
+                    },
+                    'Genres': {
+                        multi_select: [{ "name": movieData.firstGenre }, { "name": movieData.secondGenre }, { "name": movieData.thirdGenre }],
+                    },
+                    'TMDB Rating': {
+                        number: movieData.vote_average,
+                    },
+                    'TMDB Link': {
+                        url: movieData.tmdb_link,
+                    },
+                    'iMDB Link': {
+                        url: movieData.imdb_link,
+                    },
+                    'Adult': {
+                        checkbox: movieData.adult,
+                    },
+                    'Type': {
+                        select: {
+                            name: 'Movie',
+                        },
+                    },
+                    'Status': {
+                        select: {
+                            name: 'To watch',
+                        },
+                    },
                 },
-            ],
-        });
-        res.status(200).json(newPage);
+                icon: {
+                    type: 'emoji',
+                    emoji: '🎬',
+                },
+                cover: {
+                    type: 'external',
+                    external: {
+                        url: movieData.backdrop_path,
+                    },
+                },
+            });
+        } else {
+            // Create a new page
+            // ... (The rest of the code remains the same)
+            const newPage = await notion.pages.create({
+                parent: {
+                    database_id: db_id,
+                },
+                properties: {
+                    'ID': {
+                        number: movieData.id,
+                    },
+                    'Name': {
+                        title: [
+                            {
+                                text: {
+                                    content: movieData.title,
+                                },
+                            },
+                        ],
+                    },
+                    'Release Date': {
+                        date: {
+                            start: movieData.release_date,
+                        },
+                    },
+                    'Genres': {
+                        multi_select: genresArray,
+                    },
+                    'TMDB Rating': {
+                        number: movieData.vote_average,
+                    },
+                    'TMDB Link': {
+                        url: movieData.tmdb_link,
+                    },
+                    'iMDB Link': {
+                        url: movieData.imdb_link,
+                    },
+                    'Adult': {
+                        checkbox: movieData.adult,
+                    },
+                    'Type': {
+                        select: {
+                            name: 'Movie',
+                        },
+                    },
+                    'Status': {
+                        select: {
+                            name: 'To watch',
+                        },
+                    },
+                },
+                icon: {
+                    type: 'emoji',
+                    emoji: '🎬',
+                },
+                cover: {
+                    type: 'external',
+                    external: {
+                        url: movieData.backdrop_path,
+                    },
+                },
+            });
+
+            const contentUpdateResponse = await notion.blocks.children.append({
+                block_id: newPage.id,
+                children: [
+                    {
+                        object: 'block',
+                        type: 'embed',
+                        embed: {
+                            url: movieData.poster_path,
+                        },
+                    },
+                    {
+                        object: 'block',
+                        type: 'paragraph',
+                        paragraph: {
+                            rich_text: [
+                                {
+                                    type: 'text',
+                                    text: {
+                                        content: movieData.overview,
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                ],
+            });
+
+        }
+        res.status(200).json({ success: true });
     } catch (error) {
         res.status(500).json({ error: "It didn't work G" });
     }
