47e8410b42bc6f626f92d0cb6f9fbb7828dac5a4
dren.s <dren.s@gjirafa.com>
2023-05-13 14:59:20 +0200

    Changed name to clicknotes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document({ }: any) {
   return (
     <Html lang="en">
-      <title>MovieNotes</title>
-      <meta name="MovieNotes" content="Keeping track of your favourite movies, made easier" />      
+      <title>ClickNotes</title>
+      <meta name="ClickNotes" content="Keeping track of your favourite movies, made easier" />      
       <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="drensokoli" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18" defer></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js" defer></script>
 
