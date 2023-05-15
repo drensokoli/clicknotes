693b7e372c4a0b98da2024507eca3b6fb565e2cc
dren.s <dren.s@gjirafa.com>
2023-05-15 20:17:32 +0200

    Info bubble bug fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,52 +24,37 @@ function WrappedApp({ Component, pageProps, router }: WrappedAppProps) {
   const { data: session } = useSession();
   const [showInfoBubble, setShowInfoBubble] = React.useState(false);
 
-  const notionConnection = async () => {
-    const response = await fetch('/api/getUser', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ userEmail: session?.user?.email }),
-    });
-
-    const user = await response.json();
-
-    if (
-      !user?.notionApiKey ||
-      (!user?.moviesPageLink && !user?.tvShowsPageLink && !user?.booksPageLink)
-    ) {
-      setShowInfoBubble(true);
-    } else {
-      setShowInfoBubble(false);
-    }
-  };
-
   useEffect(() => {
-    if (session) {
-      notionConnection();
-    }
-  }, [session]);
+    const getUser = async () => {
+      const response = await fetch('/api/getUser', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ userEmail: session?.user?.email }),
+      });
+      const user = await response.json();
+
+      if (router.pathname === '/help' || router.pathname === '/auth/signin') {
+        setShowInfoBubble(false);
+      } else if (
+        !session ||
+        !user?.notionApiKey ||
+        (!user?.moviesPageLink && !user?.tvShowsPageLink && !user?.booksPageLink)
+      ) {
+        setShowInfoBubble(true);
+      } else {
+        setShowInfoBubble(false);
+      }
+    };
+
+    getUser();
+  }, [session, router.pathname]);
+
 
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
-      {!session && (
-        <div className="info-bubble sm:w-[300px] w-[230px]">
-          <div
-            className="info-bubble__close"
-            onClick={() => setShowInfoBubble(false)}
-          >
-            &times;
-          </div>
-          Need Help connecting?{" "}
-          <a href="/help" target="_blank">
-            <span className="text-blue-500"
-              onClick={() => setShowInfoBubble(false)}
-            >Click here</span>
-          </a>
-        </div>
-      )}
-      {showInfoBubble && router.pathname !== '/help' && (
+      {showInfoBubble && (
         <div className="info-bubble sm:w-[300px] w-[230px]">
           <div
             className="info-bubble__close"
