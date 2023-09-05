c50568d5a6f8c18080733e8724e75b349c1e5828
Dren Sokoli <dren.s@gjirafa.com>
2023-09-05 14:28:05 +0200

    Veq pe pushi

--- src/pages/saved.tsx
+++ src/pages/saved.tsx
@@ -5,8 +5,12 @@ import { decryptData } from '@/lib/crypto';
 const Saved: React.FC = () => {
   const { data: session } = useSession();
   const [pageTitles, setPageTitles] = useState<string[]>([]);
+  const [notionApiKey, setNotionApiKey] = useState('');
+  const [moviesPageLink, setMoviesPageLink] = useState('');
+  const [tvShowsPageLink, setTvShowsPageLink] = useState('');
+  const [booksPageLink, setBooksPageLink] = useState('');
 
-//   useEffect(() => {
+  useEffect(() => {
     const fetchUser = async () => {
       const response = await fetch('/api/getUser', {
         method: 'POST',
@@ -15,27 +19,42 @@ const Saved: React.FC = () => {
       });
 
       const user = await response.json();
-      const notionApiKey = decryptData(user.notionApiKey);
-      const databaseId = decryptData(user.moviesPageLink);
+      const decryptedNotionApiKey = decryptData(user.notionApiKey);
+      const decryptedMoviesPageLink = decryptData(user.moviesPageLink);
+      const decryptedTvShowsPageLink = decryptData(user.tvShowsPageLink);
+      const decryptedBooksPageLink = decryptData(user.booksPageLink);
+
+      setNotionApiKey(decryptedNotionApiKey);
+      setMoviesPageLink(decryptedMoviesPageLink);
+      setTvShowsPageLink(decryptedTvShowsPageLink);
+      setBooksPageLink(decryptedBooksPageLink);
+      
+      console.log(decryptedNotionApiKey)
+      console.log(decryptedMoviesPageLink)
+      console.log(decryptedTvShowsPageLink)
+      console.log(decryptedBooksPageLink)
 
       const pagesResponse = await fetch('/api/getNotionMovies', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ notionApiKey, databaseId }),
+        body: JSON.stringify({ 
+          notionApiKey: decryptData(user.notionApiKey),
+          databaseId: decryptData(user.moviesPageLink),
+        }),
       });
-
+      
       const data = await pagesResponse.json();
+      console.log(data)
       setPageTitles(data.pageTitles)
     }
-    fetchUser();
-//   }, []);
-console.log(pageTitles)
+    if(session){
+      fetchUser();
+    }
+  }, [session]);
 
   return (
       <>
           <div>
-              <h1>Saved</h1>
-              <p>Here are the movies you've saved to Notion.</p>
               <ul>
                 {pageTitles.map((title, index) =>
                     <li key={index}>
