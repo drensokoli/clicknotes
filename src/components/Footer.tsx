06d1b7e351fe234713e765981be625025c662949
Dren Sokoli <dren.s@gjirafa.com>
2023-09-15 15:58:36 +0200

    Updated Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,10 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-white p-4 bottom-0 w-full z-10 flex items-center justify-center">
+    <footer className="bg-white p-4 bottom-0 w-full z-10 flex flex-col items-center justify-center">
+      <h1 className='text-gray-800'>Made with ❤️ by Dren Sokoli</h1>
       <p className="text-gray-800 mr-2 ">
-        &copy; 2023{' '}
+        &copy; {new Date().getFullYear()}{' '}
         <span className="text-blue-400">
           <a href="https://github.com/drensokoli/clicknotes">ClickNotes</a>
         </span>
