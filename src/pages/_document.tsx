4aaed2f2eb1870eaed2d20c6feeb3ab6a9231ace
dren.s <dren.s@gjirafa.com>
2023-10-15 18:53:54 +0200

    ads script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,10 +9,11 @@ export default function Document({ }: any) {
       {/* <div className='hidden'>
         <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="drensokoli" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18" defer></script>
       </div> */}
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3464540666338005" crossOrigin="anonymous" defer></script>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3464540666338005" crossOrigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js" defer></script>
 
       <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3464540666338005" crossOrigin="anonymous"></script>
         <meta name="google-site-verification" content="FcZHO-KLXtlpoG78-JTPwWnFRiQ3APV-sm3VJXlLb1E" />
         <meta name="google-adsense-account" content="ca-pub-3464540666338005" />
       </Head>
@@ -20,6 +21,7 @@ export default function Document({ }: any) {
         <Main />
         <NextScript />
         <Analytics />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3464540666338005" crossOrigin="anonymous"></script>
       </body>
     </Html>
 
