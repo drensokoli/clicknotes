21f133f37ad70620f386993b80602322bc44fa09
dren.s <dren.s@gjirafa.com>
2023-10-15 18:48:09 +0200

    google verification, head tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,15 +6,16 @@ export default function Document({ }: any) {
     <Html lang="en">
       <title>ClickNotes</title>
       <meta name="ClickNotes" content="Keeping track of your favourite movies, made easier" />
-      <meta name="google-site-verification" content="FcZHO-KLXtlpoG78-JTPwWnFRiQ3APV-sm3VJXlLb1E" />
-      <meta name="google-adsense-account" content="ca-pub-3464540666338005" />
       {/* <div className='hidden'>
         <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="drensokoli" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18" defer></script>
       </div> */}
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3464540666338005" crossOrigin="anonymous" defer></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js" defer></script>
 
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="FcZHO-KLXtlpoG78-JTPwWnFRiQ3APV-sm3VJXlLb1E" />
+        <meta name="google-adsense-account" content="ca-pub-3464540666338005" />
+      </Head>
       <body>
         <Main />
         <NextScript />
