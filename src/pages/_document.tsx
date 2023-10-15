f37dcdc08e275212fe45ebf36d7d8c89f6ae8000
dren.s <dren.s@gjirafa.com>
2023-10-15 21:05:25 +0200

    ad block recovery

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import { Analytics } from '@vercel/analytics/react';
+import { useEffect } from 'react';
 
 export default function Document({ }: any) {
+  useEffect(() => {
+    function signalGooglefcPresent() {
+      if (!(window.frames as any)['googlefcPresent']) {
+        if (document.body) {
+          const iframe = document.createElement('iframe');
+          iframe.setAttribute('style', 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px; display: none;');
+          iframe.name = 'googlefcPresent';
+          document.body.appendChild(iframe);
+        } else {
+          setTimeout(signalGooglefcPresent, 0);
+        }
+      }
+    }
+    signalGooglefcPresent();
+  }, []);
+
   return (
     <Html lang="en">
       <title>ClickNotes</title>
@@ -13,6 +30,7 @@ export default function Document({ }: any) {
       <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js" defer></script>
 
       <Head>
+        <script async src="https://fundingchoicesmessages.google.com/i/pub-3464540666338005?ers=1" nonce="DV0Ctwps1CcM2EgF8YfUPg"></script>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3464540666338005" crossOrigin="anonymous"></script>
         <meta name="google-site-verification" content="FcZHO-KLXtlpoG78-JTPwWnFRiQ3APV-sm3VJXlLb1E" />
         <meta name="google-adsense-account" content="ca-pub-3464540666338005" />
@@ -24,6 +42,5 @@ export default function Document({ }: any) {
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3464540666338005" crossOrigin="anonymous"></script>
       </body>
     </Html>
-
   )
 }
