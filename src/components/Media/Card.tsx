376ca5a7466c6b81f6bc7580673ab93be151b44b
dren.s <dren.s@gjirafa.com>
2023-11-02 23:09:20 +0100

    Card styling

--- src/components/Media/Card.tsx
+++ src/components/Media/Card.tsx
@@ -20,7 +20,7 @@ export default function Card({ id, title, poster_path, release_date, link, handl
                 <div className='movie-image'>
                     {poster_path ? (
                         <Image
-                            src={poster_path} 
+                            src={poster_path}
                             width={200}
                             height={300}
                             alt={title}
@@ -30,27 +30,20 @@ export default function Card({ id, title, poster_path, release_date, link, handl
                     ) : (
                         <div className="w-[200px] h-[300px]"></div>
                     )}
-
-                    <Link href={link} passHref target='_blank' aria-label='Media'>
-                        <Image
-                            src="/share-black.png"
-                            className="arrows"
-                            alt="link"
-                            width={30}
-                            height={30}
-                            loading="lazy"
-                        />
-                    </Link>
                     <button type="button"
-                        className="movie-card-button text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+                        className="movie-card-button text-white border-2 border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+                        style={{ backdropFilter: 'blur(10px)' }}
                         onClick={() => { session ? handleAddToNotion() : signIn('google') }}
                     >
                         Add to Notion
                     </button>
                 </div>
-                <h2 className="text-l font-bold text-center text-gray-800">
-                    <span>{title} {release_date ? ` (${release_date.split('-')[0]})` : ''}</span>
-                </h2>
+
+                <Link href={link} passHref target='_blank' aria-label='Media'>
+                    <h2 className="text-l font-bold text-center text-gray-800 hover:text-blue-800 hover:underline transition-colors duration-200">
+                        <span>{title} {release_date ? ` (${release_date.split('-')[0]})` : ''}</span>
+                    </h2>
+                </Link>
             </div>
         </div>
     )
