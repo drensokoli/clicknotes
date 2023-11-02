f6874e9549df4d766407b63a181eca71874d0157
dren.s <dren.s@gjirafa.com>
2023-11-03 00:07:39 +0100

    lighthouse issues

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -30,7 +30,7 @@ export default function Navbar() {
                             <div className="relative flex h-16 justify-between items-center">
                                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                     <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-600 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                                        <span className="sr-only"></span>
+                                        <span className="sr-only">Open Main Menu</span>
                                         {open ? (
                                             <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                                         ) : (
