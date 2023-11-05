c0d6568edc3d730ac16952786a3323429d21b2c4
dren.s <dren.s@gjirafa.com>
2023-11-05 16:37:55 +0100

    meta for index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 
 const IndexPage = () => {
@@ -9,7 +10,20 @@ const IndexPage = () => {
     router.replace('/movies');
   }, []);
 
-  return null;
+  return (
+
+    <Head>
+      <title>ClickNotes</title>
+      <meta name="description" content="Save popular and trending movies, TV shows and books to your Notion list or search for your favourites. All your media in one place, displayed in beautiful Notion templates." />
+      <meta name="robots" content="all"></meta>
+      <meta property="og:title" content="ClickNotes" />
+      <meta property="og:description" content="Save popular and trending movies, TV shows and books to your Notion list or search for your favourites. All your media in one place, displayed in beautiful Notion templates." />
+      <meta property="og:image" content="https://www.clicknotes.site/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="author" content="Dren Sokoli" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
 };
 
 export default IndexPage;
