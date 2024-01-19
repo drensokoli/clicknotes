ec166c546b254f9a9c6986b35bec4fd9499c27da
Dren Sokoli <dren.s@gjirafa.com>
2024-01-19 09:29:11 +0100

    pwa related updates

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import { Analytics } from '@vercel/analytics/react';
 import { useEffect } from 'react';
+import { Metadata } from 'next';
 
 export default function Document({ }: any) {
   return (
@@ -8,6 +9,7 @@ export default function Document({ }: any) {
       <title>ClickNotes</title>
       <meta name="ClickNotes" content="Keeping track of your favourite movies, made easier" />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js" defer></script>
+      <link rel="manifest" href="manifest.webmanifest" />
 
       <Head />
       <body>
@@ -18,3 +20,25 @@ export default function Document({ }: any) {
     </Html>
   )
 }
+
+export const metadata: Metadata = {
+  title: "PWA with Next 13",
+  description: "PWA application with Next 13",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    { name: "Dren Sokoli" },
+    {
+      name: "Dren Sokoli",
+      url: "https://www.linkedin.com/in/dren-sokoli-0003a81a1/",
+    },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
+};
