696d85864b32baa718392109ef2f7ec0707b7b5f
Dren Sokoli <dren.s@gjirafa.com>
2024-02-20 16:48:26 +0100

    updated lists

--- src/pages/lists.tsx
+++ src/pages/lists.tsx
@@ -5,10 +5,10 @@ import { decryptData } from "@/lib/encryption";
 import { getMovies, getMoviesInSSR } from "@/lib/notion";
 import { set } from "lodash";
 import { getSession, useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { JSXElementConstructor, Key, ReactElement, ReactFragment, useEffect, useState } from "react";
 import { Client } from '@notionhq/client';
 
-export default function Lists({ encryptionKey, movies, books }: { encryptionKey: string, movies: any, books: any }) {
+export default function nameList({ movies, books, nameList, movieStatusList, bookStatusList }: { movies: any, books: any, nameList: any, movieStatusList: any, bookStatusList: any }) {
 
     const { data: session } = useSession();
     const [input, setInput] = useState('');
@@ -26,11 +26,44 @@ export default function Lists({ encryptionKey, movies, books }: { encryptionKey:
 
             <div className="flex flex-col items-center min-h-screen bg-white space-y-4">
                 <SearchBar input={input} handleInputChange={handleInputChange} />
-                {movies.length === 0 && (
+                <div className="flex justify-end items-center w-[90%] sm:w-[70%] gap-2">
                     <div>
-                        No movies found
+                        <select
+                            className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        // value={nameList[0].name}
+                        >
+                            {bookStatusList.map((status: any) => (
+                                <option key={status} value={status}>
+                                    {status}
+                                </option>
+                            ))}
+                        </select>
                     </div>
-                )}
+                    <div>
+                        <select
+                            className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        // value={nameList[0].name}
+                        >
+                            {movieStatusList.map((status: any) => (
+                                <option key={status} value={status}>
+                                    {status}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                        <select
+                            className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        // value={nameList[0].name}
+                        >
+                            {nameList.map((list: { databaseName: any; }) => (
+                                <option key={list.databaseName} value={list.databaseName}>
+                                    {list.databaseName}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
                 <div className="content-container sm:w-5/6">
                     <div className="movie-container grid grid-cols-2 gap-2 sm:grid-cols-1">
                         {movies && (
@@ -80,7 +113,7 @@ export const getServerSideProps = async (context: any) => {
         const user = await response.json();
 
         if (!user.notionApiKey)
-            return { movies: [], books: [] };
+            return { movies: [], moviesDatabaseName: '', books: [], booksDatabaseName: '' };
 
         const notionApiKey = user.notionApiKey;
         const decryptedNotionApiKey = decryptData(notionApiKey, encryptionKey);
@@ -89,8 +122,19 @@ export const getServerSideProps = async (context: any) => {
 
         let moviesResponse, booksResponse;
 
+        const nameList = [];
+        let movieStatusList = [] as any;
+        let bookStatusList = [] as any;
+
         if (user.moviesPageLink) {
             const decryptedMoviesPageLink = decryptData(user.moviesPageLink, encryptionKey);
+            const moviesDatabaseInfo = await notion.databases.retrieve({ database_id: decryptedMoviesPageLink }) as any;
+
+            const moviesDatabaseName = moviesDatabaseInfo.icon.emoji + moviesDatabaseInfo.title[0].plain_text;
+            nameList.push({ databaseName: moviesDatabaseName });
+
+            movieStatusList = moviesDatabaseInfo.properties.Status.select.options.map((status: any) => status.name);
+
             moviesResponse = await notion.databases.query({
                 database_id: decryptedMoviesPageLink
             });
@@ -100,6 +144,13 @@ export const getServerSideProps = async (context: any) => {
 
         if (user.booksPageLink) {
             const decryptedBooksPageLink = decryptData(user.booksPageLink, encryptionKey);
+            const booksDatabaseInfo = await notion.databases.retrieve({ database_id: decryptedBooksPageLink }) as any;
+
+            const booksDatabaseName = booksDatabaseInfo.icon.emoji + booksDatabaseInfo.title[0].plain_text;
+            nameList.push({ databaseName: booksDatabaseName });
+
+            bookStatusList = booksDatabaseInfo.properties.Status.select.options.map((status: any) => status.name);
+
             booksResponse = await notion.databases.query({
                 database_id: decryptedBooksPageLink
             });
@@ -107,16 +158,24 @@ export const getServerSideProps = async (context: any) => {
             booksResponse = { results: [] };
         }
 
-        return { movies: moviesResponse.results, books: booksResponse.results };
+        return {
+            movies: moviesResponse.results,
+            books: booksResponse.results,
+            nameList,
+            movieStatusList,
+            bookStatusList
+        };
     };
 
-    const { movies, books } = await fetchUser();
+    const { movies, books, nameList, movieStatusList, bookStatusList } = await fetchUser();
 
     return {
         props: {
-            encryptionKey,
             movies,
             books,
+            nameList,
+            movieStatusList,
+            bookStatusList
         },
     };
 }
