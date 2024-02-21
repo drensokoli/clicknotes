b3a160c71f23e250f2b75c86ebd2a04d7ba268d8
Dren Sokoli <dren.s@gjirafa.com>
2024-02-21 09:20:11 +0100

    updated toast

--- src/components/Helpers/Toast.tsx
+++ src/components/Helpers/Toast.tsx
@@ -9,7 +9,7 @@ export default function Toast({ apiResponse, setApiResponse, pageLink }: { apiRe
         }, 5000);
 
         return (
-            <div id="toast-success" className="flex items-center w-full max-w-xs p-4 text-gray-800 bg-white border-2 border-green-400 fixed left-5 bottom-8 z-20 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+            <div id="toast-success" className="flex items-center w-fit max-w-xs p-4 text-gray-800 bg-white border-2 border-green-400 fixed left-5 bottom-5 z-20 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
                 <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
@@ -17,7 +17,7 @@ export default function Toast({ apiResponse, setApiResponse, pageLink }: { apiRe
                     <span className="sr-only">Check icon</span>
                 </div>
                 <div>
-                    <div className="ml-3 text-md font-normal">{apiResponse}</div>
+                    <div className="ml-3 text-md font-normal mr-4">{apiResponse}</div>
                     <div className="ml-3 text-sm font-normal"><a href={pageLink} target='_blank' className='text-blue-700 underline font-semibold'>Go to page</a></div>
                 </div>
                 <button type="button"
@@ -38,7 +38,7 @@ export default function Toast({ apiResponse, setApiResponse, pageLink }: { apiRe
         }, 5000);
 
         return (
-            <div id="toast-danger" className="flex items-center w-full max-w-xs p-4 text-gray-800 bg-white border-2 border-red-400 fixed left-5 bottom-8 z-20 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+            <div id="toast-danger" className="flex items-center w-fit max-w-xs p-4 text-gray-800 bg-white border-2 border-red-400 fixed left-5 bottom-5 z-20 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
                 <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z" />
@@ -46,7 +46,7 @@ export default function Toast({ apiResponse, setApiResponse, pageLink }: { apiRe
                     <span className="sr-only">Error icon</span>
                 </div>
                 <div>
-                    <div className="ml-3 text-sm font-normal">{apiResponse}</div>
+                    <div className="ml-3 text-sm font-normal mr-4">{apiResponse}</div>
                     <div className="ml-3 text-sm font-normal">
                         <Link href="/help" target="_blank" aria-label='Help page'>
                             Need <span className="text-blue-700 underline font-semibold">help</span> ?
@@ -65,14 +65,14 @@ export default function Toast({ apiResponse, setApiResponse, pageLink }: { apiRe
         );
     } else if (apiResponse === 'Adding movie to Notion...' || apiResponse === 'Adding TV show to Notion...' || apiResponse === 'Adding book to Notion...') {
         return (
-            <div id="toast-danger" className="flex items-center w-full max-w-xs p-4 text-gray-800 bg-white border-2 border-blue-400 fixed left-5 bottom-8 z-20 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+            <div id="toast-danger" className="flex items-center w-fit max-w-xs p-4 text-gray-800 bg-white border-2 border-blue-400 fixed left-5 bottom-5 z-20 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
                 <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-blue-500 rounded-lg dark:bg-blue-800 dark:text-blue-200">
                     <svg className="w-5 h-5 text-blue-600 dark:text-blue-500 rotate-45" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m9 17 8 2L9 1 1 19l8-2Zm0 0V9" />
                     </svg>
                     <span className="sr-only">Adding</span>
                 </div>
-                <div className="ml-3 text-sm font-normal">{apiResponse}</div>
+                <div className="ml-3 text-sm font-normal mr-4">{apiResponse}</div>
             </div>
         );
     } else if (apiResponse === 'User data updated successfully') {
@@ -82,14 +82,14 @@ export default function Toast({ apiResponse, setApiResponse, pageLink }: { apiRe
         }, 5000);
 
         return (
-            <div id="toast-success" className="flex items-center w-full max-w-xs p-4 text-gray-800 bg-white border-2 border-green-400 fixed left-5 bottom-8 z-20 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+            <div id="toast-success" className="flex items-center w-fit max-w-xs p-4 text-gray-800 bg-white border-2 border-green-400 fixed left-5 bottom-5 z-20 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
                 <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                     </svg>
                     <span className="sr-only">Check icon</span>
                 </div>
-                <div className="ml-3 text-sm font-normal">{apiResponse}</div>
+                <div className="ml-3 text-sm font-normal mr-4">{apiResponse}</div>
                 <button type="button"
                     onClick={() => setApiResponse(null)}
                     className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-success" aria-label="Close">
@@ -107,14 +107,14 @@ export default function Toast({ apiResponse, setApiResponse, pageLink }: { apiRe
         }, 5000);
 
         return (
-            <div id="toast-danger" className="flex items-center w-full max-w-xs p-4 text-gray-800 bg-white border-2 border-red-400 fixed left-5 bottom-8 z-20 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+            <div id="toast-danger" className="flex items-center w-fit max-w-xs p-4 text-gray-800 bg-white border-2 border-red-400 fixed left-5 bottom-5 z-20 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
                 <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z" />
                     </svg>
                     <span className="sr-only">Error icon</span>
                 </div>
-                <div className="ml-3 text-sm font-normal">{apiResponse}</div>
+                <div className="ml-3 text-sm font-normal mr-4">{apiResponse}</div>
                 <button type="button"
                     onClick={() => setApiResponse(null)}
                     className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
