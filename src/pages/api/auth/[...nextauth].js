f385cd8424c01b713e527d76afaa1e13d66cac7d
Dren Sokoli <drensokoli@gmail.com>
2024-03-01 16:36:17 +0100

    allowDangerousEmailAccountLinking

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -18,6 +18,7 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
   secret: process.env.JWT_SECRET
