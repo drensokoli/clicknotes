fa5fcafbce620d470c96218487296c0821e4a32c
Dren Sokoli <drensokoli@gmail.com>
2024-03-08 20:13:12 +0100

    minor update

--- src/components/Lists/ListSkeleton.tsx
+++ src/components/Lists/ListSkeleton.tsx
@@ -1,7 +1,7 @@
 export default function ListsSkeleton() {
   return (
     <div className='grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 sm:gap-4'>
-      {Array(15).fill(0).map((_, index) => (
+      {Array(10).fill(0).map((_, index) => (
         <div className="w-[200px] px-4 sm:px-0 py-2 sm:py-0" key={index}>
           <div className="flex items-end justify-center bg-gray-100 rounded-sm max-h-[240px] min-h-[240px] sm:max-h-[300px] sm:min-h-[300px] h-auto select-none object-cover">
             <div className="border-2 border-gray-350 bg-gray-200 rounded-lg h-12 w-[90%] mb-3 animate-pulse"></div>
--- src/pages/connect.tsx
+++ src/pages/connect.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useSession, getSession } from 'next-auth/react';
 import Image from 'next/dist/client/image';
-import { decryptData } from '../lib/encryption';
 import 'flowbite';
-import { notionApiKeySubmit, moviesLinkSubmit, tvShowsLinkSubmit, booksLinkSubmit, extractValueFromUrl } from '../lib/profileHelpers';
+import { extractValueFromUrl } from '../lib/profileHelpers';
 import Head from 'next/head';
 import Link from 'next/link';
 import Toast from '@/components/Helpers/Toast';
@@ -40,6 +39,7 @@ export default function Connect() {
             setApiResponse('Please enter a valid Notion link');
             return 'Please enter a valid Notion link';
         }
+
         databaseId = extractValueFromUrl(databaseId);
 
         const response = await fetch('/api/updateUserConnection', {
@@ -115,62 +115,63 @@ export default function Connect() {
                     crossOrigin="anonymous"></script>
             </Head>
             <Toast apiResponse={apiResponse} setApiResponse={setApiResponse} pageLink={undefined} />
-            <div className="flex justify-center items-center flex-grow">
+            <div className='min-h-screen'>
 
-                <Transition
-                    className="bg-white relative mx-auto rounded-md md:w-[50%] w-[90%] shadow-xl  border-gray-100 border-2"
-                    show={show}
-                    enter="transition-all ease-in-out duration-500 delay-[200ms]"
-                    enterFrom="opacity-0 translate-y-6"
-                    enterTo="opacity-100 translate-y-0"
-                    leave="transition-all ease-in-out duration-300"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
-                    <div className="flex justify-center mt-4">
-                        <Image src={session?.user?.image?.toString()!} alt="" className="rounded-full mx-auto w-32 h-32 shadow-2xl border-4 border-white transition duration-200 transform hover:scale-110 select-none" width={50} height={50} />
-                    </div>
-                    <h1 className="font-bold text-center text-3xl text-gray-700 mt-2">{session?.user?.name}</h1>
-                    <div className="w-full flex flex-col">
-                        <div className='flex flex-col my-4 mx-8'>
-                            <label htmlFor="" className='block mb-2 text-sm text-gray-400'>Movies and TV Shows Integration Token</label>
-                            <h1 className='text-gray-500 mb-4 w-full truncate pr-4 text-sm'>{moviesApiKey ? moviesApiKey : "You don't have a Movies and TV shows connection"}</h1>
-                            <Input
-                                label='Movies and TV Shows Database'
-                                placeHolder='Enter the database ID'
-                                field={moviesDatabaseId}
-                                link={'https://www.notion.so/' + moviesDatabaseId}
-                                setLink={setMoviesDatabaseId}
-                                setInput={setInput}
-                                handleSubmit={(e: any) => handleDatabaseIdSubmit(e, 'movies')}
-                                connectionType='movies'
-                            />
-                            <div className='flex justify-center items-center'>
-                                <Link href={moviesNotionAuthUrl} className="w-[400px] text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Connect Movies and Shows</Link>
+                <div className="flex justify-center items-center flex-grow">
+                    <Transition
+                        className="bg-white relative mx-auto rounded-md md:w-[50%] w-[90%] shadow-xl  border-gray-100 border-2"
+                        show={show}
+                        enter="transition-all ease-in-out duration-500 delay-[200ms]"
+                        enterFrom="opacity-0 translate-y-6"
+                        enterTo="opacity-100 translate-y-0"
+                        leave="transition-all ease-in-out duration-300"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="flex justify-center mt-4">
+                            <Image src={session?.user?.image?.toString()!} alt="" className="rounded-full mx-auto w-32 h-32 shadow-2xl border-4 border-white transition duration-200 transform hover:scale-110 select-none" width={50} height={50} />
+                        </div>
+                        <h1 className="font-bold text-center text-3xl text-gray-700 mt-2">{session?.user?.name}</h1>
+                        <div className="w-full flex flex-col">
+                            <div className='flex flex-col my-4 mx-8'>
+                                <label htmlFor="" className='block mb-2 text-sm text-gray-400'>Movies and TV Shows Integration Token</label>
+                                <h1 className='text-gray-500 mb-4 w-full truncate pr-4 text-sm'>{moviesApiKey ? moviesApiKey : "You don't have a Movies and TV shows connection"}</h1>
+                                <Input
+                                    label='Movies and TV Shows Database'
+                                    placeHolder='Enter the database ID'
+                                    field={moviesDatabaseId}
+                                    link={'https://www.notion.so/' + moviesDatabaseId}
+                                    setLink={setMoviesDatabaseId}
+                                    setInput={setInput}
+                                    handleSubmit={(e: any) => handleDatabaseIdSubmit(e, 'movies')}
+                                    connectionType='movies'
+                                />
+                                <div className='flex justify-center items-center'>
+                                    <Link href={moviesNotionAuthUrl} className="w-[400px] text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Connect Movies and Shows</Link>
+                                </div>
+                            </div>
+                            <hr className='mx-8' />
+                            <div className='flex flex-col my-4 mx-8'>
+                                <label htmlFor="" className='block mb-2 text-sm text-gray-400'>Books Integration Token</label>
+                                <h1 className='text-gray-500 mb-4 w-full truncate pr-4 text-sm'>{booksApiKey ? booksApiKey : "You don't have a Books connection"}</h1>
+                                <Input
+                                    label='Books Database'
+                                    placeHolder='Enter the database ID'
+                                    field={booksDatabaseId}
+                                    link={'https://www.notion.so/' + booksDatabaseId}
+                                    setLink={setBooksDatabaseId}
+                                    setInput={setInput}
+                                    handleSubmit={(e: any) => handleDatabaseIdSubmit(e, 'books')}
+                                    connectionType='books'
+                                />
+                                <div className='flex justify-center items-center'>
+                                    <Link href={booksNotionAuthUrl} className="w-[400px] text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Connect Books</Link>
+                                </div>
                             </div>
                         </div>
-                        <hr className='mx-8' />
-                        <div className='flex flex-col my-4 mx-8'>
-                            <label htmlFor="" className='block mb-2 text-sm text-gray-400'>Books Integration Token</label>
-                            <h1 className='text-gray-500 mb-4 w-full truncate pr-4 text-sm'>{booksApiKey ? booksApiKey : "You don't have a Books connection"}</h1>
-                            <Input
-                                label='Books Database'
-                                placeHolder='Enter the database ID'
-                                field={booksDatabaseId}
-                                link={'https://www.notion.so/' + booksDatabaseId}
-                                setLink={setBooksDatabaseId}
-                                setInput={setInput}
-                                handleSubmit={(e: any) => handleDatabaseIdSubmit(e, 'books')}
-                                connectionType='books'
-                            />
-                            <div className='flex justify-center items-center'>
-                                <Link href={booksNotionAuthUrl} className="w-[400px] text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Connect Books</Link>
-                            </div>
-                        </div>
-                    </div>
-                </Transition>
+                    </Transition>
+                </div>
             </div>
-
         </>
     );
 }
